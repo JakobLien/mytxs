9f85a9b31116714f5fcf7b98d5e6b53935b3207a
Jakob Lien <jakobli@stud.ntnu.no>
2023-05-01 21:30:06 +0200

    ignore me

--- mytxs/settings.py
+++ mytxs/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 
 import dotenv
 
+dotenv.load_dotenv(dotenv.find_dotenv())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,9 +78,13 @@ WSGI_APPLICATION = 'mytxs.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ['DATABASE_ENGINE'],
+        "NAME": os.environ['DATABASE_NAME'],
+        "USER": os.environ['DATABASE_USER'],
+        "PASSWORD": os.environ['DATABASE_PASSWORD'],
+        "HOST": os.environ['DATABASE_HOST'],
+        "PORT": os.environ['DATABASE_PORT'],
     }
 }
 
@@ -143,16 +149,3 @@ INTERNAL_IPS = [
 # Media files, uploaded by user
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
-
-dotenv.load_dotenv(dotenv.find_dotenv())
-
-DATABASES = {
-    "default": {
-        "ENGINE": os.environ['DATABASE_ENGINE'],
-        "NAME": os.environ['DATABASE_NAME'],
-        "USER": os.environ['DATABASE_USER'],
-        "PASSWORD": os.environ['DATABASE_PASSWORD'],
-        "HOST": os.environ['DATABASE_HOST'],
-        "PORT": os.environ['DATABASE_PORT'],
-    }
-}
