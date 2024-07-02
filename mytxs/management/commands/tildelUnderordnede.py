c101668c0ffd8f4799a502958c3abbc5bf7af488
Ole Johan Walderhaug <olejodd10@gmail.com>
2024-07-02 21:56:08 +0200

    Legge til tildelUnderordnede management command

---
+++ mytxs/management/commands/tildelUnderordnede.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+
+from mytxs.management.commands.updateField import objectsGenerator
+from mytxs.models import DekorasjonInnehavelse
+from mytxs.utils.modelUtils import manglerUnderordnet
+
+
+def tildelAlleUnderordnede(dekorasjonInnehavelse):
+    '''
+    Tildeler alle manglende underordnede dekorasjoner.
+    Lagring av nye innehavelser gjøres i omvendt topologisk sortert 
+    rekkefølge (laveste valør først) for å passere validering.
+    '''
+    nyeInnehavelser = []
+    nåværende = dekorasjonInnehavelse
+    while manglerUnderordnet(nåværende):
+        ny = DekorasjonInnehavelse(
+            medlem=nåværende.medlem,
+            dekorasjon=nåværende.dekorasjon.erOverordnet,
+            start=nåværende.start
+        )
+        nyeInnehavelser.append(ny)
+        nåværende = ny
+    for innehavelse in reversed(nyeInnehavelser):
+        innehavelse.save()
+
+
+class Command(BaseCommand):
+    help = 'tildel underordnede dekorasjoner til alle medlemmer som mangler det'
+
+    def handle(self, *args, **options):
+        for instance in objectsGenerator(DekorasjonInnehavelse.objects.all()):
+            tildelAlleUnderordnede(instance)
