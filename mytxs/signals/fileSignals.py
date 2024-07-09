ab856ff482aa2251300ae6d3cda7689f2ee6628c
Jakob Lien <jakobli@stud.ntnu.no>
2024-07-09 19:43:58 +0200

    Generalized fileSignals.py

--- mytxs/signals/fileSignals.py
+++ mytxs/signals/fileSignals.py
@@ -1,32 +1,33 @@
 import os
-from django.dispatch import receiver
+from django.apps import apps
 from django.db.models.signals import post_delete, pre_save
+from django.db.models import FileField
 
 from mytxs.models import Medlem
+from mytxs.signals.logSignals import recieverWithModels
 
-# Håndtering av opplastning og overskriving av bilder
-# Gjør at ImageField lagres som nr i rett mappe
+fileModels = [m for m in apps.get_models() if any(map(lambda f: isinstance(f, FileField), m._meta.get_fields()))]
+
 # https://stackoverflow.com/a/16041527
-@receiver(post_delete, sender=Medlem)
+@recieverWithModels(post_delete, senders=fileModels)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
-    'Slette Medlem bilde når medlemmet slettes'
-    if instance.bilde:
-        os.remove(instance.bilde.path)
+    'Slette en fil når instansen i databasen slettes'
+    for field in sender._meta.get_fields():
+        if isinstance(field, FileField):
+            if fil := getattr(instance, field.name):
+                if os.path.isfile(fil.path):
+                    os.remove(fil.path)
 
-
-@receiver(pre_save, sender=Medlem)
+@recieverWithModels(pre_save, senders=fileModels)
 def auto_delete_file_on_change(sender, instance, **kwargs):
-    '''
-    Slette Medlem bilde fra filsystemet når de laste opp et nytt bilde.
-    Om ikke dette gjøres får filnavnet (medlem.pk) ekstra characters på slutten,
-    som ikke endre koss det funke for resten av appen, men ser stygt ut.
-    '''
-    try:
-        gammeltBilde = Medlem.objects.get(pk=instance.pk).bilde
-    except Medlem.DoesNotExist:
-        return
+    'Slette gamle fila når en ny fil lastes opp, for å unngå random characters på slutten'
+    for field in sender._meta.get_fields():
+        if isinstance(field, FileField):
+            try:
+                gammelFil = getattr(Medlem.objects.get(pk=instance.pk), field.name)
+            except sender.DoesNotExist:
+                return
 
-    new_file = instance.bilde
-    if gammeltBilde and not gammeltBilde == new_file:
-        if os.path.isfile(gammeltBilde.path):
-            os.remove(gammeltBilde.path)
+            if gammelFil and gammelFil != getattr(instance, field.name):
+                if os.path.isfile(gammelFil.path):
+                    os.remove(gammelFil.path)
